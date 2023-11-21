9976083b4e4e9a7e30e20da21c12fb16f878a598
CodeX-cmd <kuvizic.marko@gmail.com>
2023-11-21 22:44:39 +0100

    [Add] Added user reports

---
+++ Nomad Server/src/main/java/model/UserReport.java
@@ -0,0 +1,55 @@
+package model;
+
+public class CommentReport {
+    private User reportingUser;
+    private Comment reportedComment;
+    private String reason;
+    private ReportStatus reportStatus;
+
+    // Enum to represent report status: pending, archived, accepted
+    public enum ReportStatus {
+        PENDING, ARCHIVED, ACCEPTED
+    }
+
+    // Constructor
+    public CommentReport(User reportingUser, Comment reportedComment, String reason, ReportStatus reportStatus) {
+        this.reportingUser = reportingUser;
+        this.reportedComment = reportedComment;
+        this.reason = reason;
+        this.reportStatus = reportStatus;
+    }
+
+    // Getters and setters for each attribute
+    public User getReportingUser() {
+        return reportingUser;
+    }
+
+    public void setReportingUser(User reportingUser) {
+        this.reportingUser = reportingUser;
+    }
+
+    public Comment getReportedComment() {
+        return reportedComment;
+    }
+
+    public void setReportedComment(Comment reportedComment) {
+        this.reportedComment = reportedComment;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public ReportStatus getReportStatus() {
+        return reportStatus;
+    }
+
+    public void setReportStatus(ReportStatus reportStatus) {
+        this.reportStatus = reportStatus;
+    }
+
+}
